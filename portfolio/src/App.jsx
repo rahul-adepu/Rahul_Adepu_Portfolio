160ebf1b408a2160abceaa4bd0b12a9ab0be9a97
Rahul Adepu <rahuladepu002@gmail.com>
2025-03-12 19:11:50 +0530

    Added Navbar, Intro, About section

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import About from './Components/About';
+import Intro from './Components/Intro';
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Intro />
+      <About />
+    </div>
+  );
+}
+
+export default App;
