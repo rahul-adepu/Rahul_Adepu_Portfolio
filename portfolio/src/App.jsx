035715cf45aab45ed8e50d0606ba29965d674c18
Rahul Adepu <rahuladepu002@gmail.com>
2025-04-03 11:46:34 +0530

    Implemented scroll to top functionality

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import './App.css';
 import About from './Components/About';
 import Contactme from './Components/Contactme';
@@ -6,16 +7,26 @@ import Intro from './Components/Intro';
 import Nav from './Components/Nav';
 import Projects from './Components/Projects';
 import Techstacks from './Components/TechStacks';
+import ScrollToTopButton from './Components/Scroll';
 
 function App() {
+
+  const divRef = useRef(null);
+
+  const handleScroll = () => {
+    const scrollTop = divRef.current.scrollTop;
+    console.log(scrollTop);
+  };
+
   return (
     <div className="App">
       <Nav />
       <Intro />
-      <About />
+      <About ref={divRef} func={handleScroll} />
       <Techstacks />
       <Projects />
       <Contactme />
+      <ScrollToTopButton />
       <Footer />
     </div>
   );
